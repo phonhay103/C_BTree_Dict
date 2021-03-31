fe72872a89a9f6a6eb9436d1fc3b3bb643fec1a4
phonhay103 <49369395+phonhay103@users.noreply.github.com>
2021-03-31 21:15:15 +0700

    Add files via upload

---
+++ lib/soundex.h
@@ -0,0 +1,11 @@
+#ifndef __SOUNDEX_H__
+#define __SOUNDEX_H__
+
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void soundex(const char* name, char* s);
+
+#endif
